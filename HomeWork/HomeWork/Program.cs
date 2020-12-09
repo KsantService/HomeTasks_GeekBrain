21d3daa123fcffc088ca58cf2d467cfb44e72bc2
Ksant <ksantservice@yandex.ru>
2020-12-09 21:07:18 +0300

    Home task about user name and date

---
+++ HomeWork/HomeWork/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Задайте имя пользователя, пожалуйста:");
+      string userName = Console.ReadLine();
+      Console.WriteLine($"Привет, {userName}, сегодня " + DateTime.Now.ToString("D"));
+      Console.ReadLine();
+    }
+  }
+}
