7bb927d4007726d7a340d9b5545a1b8f16acea9a
Ksant <ksantservice@yandex.ru>
2020-12-13 21:40:22 +0300

    home work 2

---
+++ HomeWork/Exercise2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+  class Program
+  {
+
+    enum Month
+    {
+      January = 1,
+      February,
+      March,
+      April,
+      May,
+      June,
+      July,
+      August,
+      September,
+      October,
+      November,
+      December
+    }
+
+    static void Main(string[] args)
+    {
+      int monthNumber;
+
+      Console.WriteLine("Задайте порядковый номер текущего месяца, пожалуйста:");
+
+      if (!Int32.TryParse(Console.ReadLine(), out monthNumber) || monthNumber < 1 || monthNumber > 12)
+      {
+        Console.WriteLine("Задано некорректное значение порядкового номера месяца. Значение должно быть целым числом от 1 до 12");
+        Console.ReadKey();
+        return;
+      }
+
+      Month currentMonth = (Month) monthNumber;
+      Console.WriteLine($"Текущий месяц {currentMonth}");
+      Console.ReadKey();
+    }
+  }
+}
---
+++ HomeWork/HomeWork/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int minTemperature, maxTemperature;
+      Console.WriteLine("Задайте минимальную температуру за сутки, пожалуйста:");
+
+      if (!Int32.TryParse(Console.ReadLine(), out minTemperature))
+      {
+        Console.WriteLine("Задано некорректное значение минимальной температуры. Значение должно быть целым числом");
+        Console.ReadKey();
+        return;
+      }
+
+      if (minTemperature < -273)
+      {
+        Console.WriteLine("Задано некорректное значение минимальной температуры. Значение не может быть меньше -273");
+        Console.ReadKey();
+        return;
+      }
+
+      Console.WriteLine("Задайте максимальную температуру за сутки, пожалуйста:");
+      if (!Int32.TryParse(Console.ReadLine(), out maxTemperature))
+      {
+        Console.WriteLine("Задано некорректное значение максимальной температуры. Значение должно быть целым числом");
+        Console.ReadKey();
+        return;
+      }
+
+      if (maxTemperature < minTemperature)
+      {
+        Console.WriteLine("Задано некорректное значение максимальной температуры. Значение не может быть меньше минимальной температуры");
+        Console.ReadKey();
+        return;
+      }
+
+      double averageTemperature = (minTemperature + maxTemperature) / 2.0;
+      Console.WriteLine($"Среднесуточная температура {averageTemperature}");
+      Console.ReadKey();
+    }
+  }
+}
