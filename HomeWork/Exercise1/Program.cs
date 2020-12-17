6fbe9a3194cd4f2562848fb6abff747c24c8a0f4
Ksant <ksantservice@yandex.ru>
2020-12-18 00:43:01 +0300

    home work 3

---
+++ HomeWork/Exercise1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int rowCount,columnCount;
+
+      Console.WriteLine("Задайте число строк в матрице, пожалуйста:");
+      while (!Int32.TryParse(Console.ReadLine(), out rowCount)||rowCount<=0)
+      {
+        Console.WriteLine("Задано некорректное значение числа строк. Значение должно быть целым положительным числом");
+      }
+
+      Console.WriteLine("Задайте число столбцов в матрице, пожалуйста:");
+      while (!Int32.TryParse(Console.ReadLine(), out columnCount)||columnCount<=0)
+      {
+        Console.WriteLine("Задано некорректное значение числа столбцов. Значение должно быть целым положительным числом");
+      }
+
+      Console.WriteLine($"Cтроим случайную матрицу размера {rowCount}x{columnCount}");
+
+      Random random = new Random();
+      int[,] matrix = new int[rowCount,columnCount];
+      for (int i = 0; i < rowCount; i++)
+      {
+        for (int j = 0; j < columnCount; j++)
+        {
+          matrix[i, j] = random.Next(100);
+          Console.Write($"{matrix[i,j]}\t");
+        }
+        Console.WriteLine();
+      }
+
+      Console.WriteLine("Диагональные элементы матрицы:");
+      int minDim = Math.Min(rowCount, columnCount);
+      for (int i = 0; i < minDim; i++)
+      {
+        Console.Write($"{matrix[i,i]} ");
+      }
+ 
+      Console.ReadKey();
+    }
+  }
+}
---
+++ HomeWork/Exercise2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string[,] catalog = new string[5, 2];
+
+      catalog[0, 0] = "Петя Васечкин";
+      catalog[0, 1] = "vasechkin@gmail.com";
+
+      catalog[1, 0] = "Энди Уорхолл";
+      catalog[1, 1] = "+19234929342";
+
+      catalog[2, 0] = "Жириновский Владимир Вольфович";
+      catalog[2, 1] = "skolen@mail.ru";
+
+      catalog[3, 0] = "Kylie Minogue";
+      catalog[3, 1] = "+19234929344";
+
+      catalog[4, 0] = "ООО \"Рога и Копыта, ltd\"";
+      catalog[4, 1] = "handh@yahoo.com";
+
+      for (int i = 0; i < catalog.GetLength(0); i++)
+      {
+        Console.Write($"{catalog[i,0]}:    {catalog[i,1]}");
+        Console.WriteLine();
+      }
+      Console.ReadKey();
+    }
+  }
+}
